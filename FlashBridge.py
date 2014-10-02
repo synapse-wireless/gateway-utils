1d972f685b7e12a828f2b38f612e4a49cd162ea6
Jonathan Creekmore <jonathan.creekmore@synapse-wireless.com>
2014-10-02 10:18:14 -0500

    Fix up some pylint issues

--- FlashBridge.py
+++ FlashBridge.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""Upgrades SNAP core, erase script files, and factory defaults
+NVParameters for attached bridge nodes"""
 import os
 import array
 import binascii
@@ -8,20 +10,16 @@ import bz2
 import sys
 from cStringIO import StringIO
 
-#
-# For Erasing Snappy Images
-#
-
 # MAGIC KEY related
 MAGIC_KEY_CMD_DEFAULT_NV = 'N'
 MAGIC_KEY_CMD_ERASE_SCRIPT = 'S'
 
 
-def buildMagicHRec(cmdChar, addr='02F0'):
+def build_magic_hrec(cmd, addr='02F0'):
     """Build a magical Intel Hex file to erase
     scripts or default NV parameters"""
-    magicKey = '524D44454D474B42'
-    record = '09%s00%s%02X' % (addr, magicKey, ord(cmdChar))
+    magic = '524D44454D474B42'
+    record = '09%s00%s%02X' % (addr, magic, ord(cmd))
     crc = (~sum(array.array('B', binascii.unhexlify(record)))+1) % 2**8
 
     text = ':%s%02X\n' % (record, crc)
@@ -29,7 +27,7 @@ def buildMagicHRec(cmdChar, addr='02F0'):
     return text
 
 
-def parseArgs():
+def parse_args():
     """Parse the arguments passed in on the command line
     to determine which function to perform"""
 
@@ -37,48 +35,48 @@ def parseArgs():
  Usage:  FlashBridge.py -i [imagename] -p [port]""")
 
     parser.add_option("-e", "--erase", dest="erase", default=False,
-                                action="store_true", 
-                                help="Erase the current SnapPy script.")
+                      action="store_true",
+                      help="Erase the current SnapPy script.")
     parser.add_option("-i", "--image", dest="image", default=None,
-                                metavar="imageName", action="store",
-                                help="The image file to flash.")
+                      metavar="imageName", action="store",
+                      help="The image file to flash.")
     parser.add_option("-n", "--defaultnv", dest="defaultnv",
-                                action="store_true", default=False,
-                                help="Reset the device's NV params.")
+                      action="store_true", default=False,
+                      help="Reset the device's NV params.")
 
     parser.add_option("-p", "--port", dest="port", metavar="comport",
-                        action="store", default='/dev/ttyS1',
-                        help="Required:  The serial device to use.")
+                      action="store", default='/dev/ttyS1',
+                      help="Required:  The serial device to use.")
 
-    (options, args) = parser.parse_args()
-    
+    (options, _) = parser.parse_args()
+
     if not (options.erase or options.image or options.defaultnv):
-        print "Must specify either -e, -i, or -n" 
+        print "Must specify either -e, -i, or -n"
         sys.exit(1)
-    
+
     return options
 
 
 if __name__ == '__main__':
-    args = parseArgs()
+    ARGS = parse_args()
 
-    if args.erase:
-        fp = StringIO(buildMagicHRec(MAGIC_KEY_CMD_ERASE_SCRIPT))
+    if ARGS.erase:
+        FP = StringIO(build_magic_hrec(MAGIC_KEY_CMD_ERASE_SCRIPT))
         print "Erase"
-    elif args.defaultnv:
-        fp = StringIO(buildMagicHRec(MAGIC_KEY_CMD_DEFAULT_NV))
+    elif ARGS.defaultnv:
+        FP = StringIO(build_magic_hrec(MAGIC_KEY_CMD_DEFAULT_NV))
         print "Default NV"
     else:
         # Assume that the file is an SFI file
-        fp = bz2.BZ2File(args.image, 'r')
+        FP = bz2.BZ2File(ARGS.image, 'r')
         try:
             # Try to go to the end of the file
-            fp.seek(-1, os.SEEK_END)
+            FP.seek(-1, os.SEEK_END)
             # And move back to the beginning
-            fp.seek(0, os.SEEK_SET)
+            FP.seek(0, os.SEEK_SET)
         except IOError:
             # If we got an IOError, assume we were not an SFI file
             # and open the file normally
-            fp = open(args.image, 'rb')
+            FP = open(ARGS.image, 'rb')
 
-    RF200Flasher.flash(fp, args.port)
+    RF200Flasher.flash(FP, ARGS.port)
